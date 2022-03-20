01574499c85b16d079173e980ee62e764a23c4c1
akzzy <akkzzy@gmail.com>
2022-03-20 10:56:44 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,13 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crosshatch.mk \
-    $(LOCAL_DIR)/aosp_blueline.mk \
-    $(LOCAL_DIR)/aosp_crosshatch_hwasan.mk \
-    $(LOCAL_DIR)/aosp_blueline_hwasan.mk \
-    $(LOCAL_DIR)/vf/aosp_crosshatch_vf.mk \
+    $(LOCAL_DIR)/derp_crosshatch.mk \
+    $(LOCAL_DIR)/derp_blueline.mk \
+    $(LOCAL_DIR)/derp_crosshatch_hwasan.mk \
+    $(LOCAL_DIR)/derp_blueline_hwasan.mk \
+    $(LOCAL_DIR)/vf/derp_crosshatch_vf.mk \
 
 COMMON_LUNCH_CHOICES := \
-    aosp_crosshatch-userdebug \
-    aosp_blueline-userdebug \
-    aosp_crosshatch_vf-userdebug
+    derp_crosshatch-userdebug \
+    derp_blueline-userdebug \
+    derp_crosshatch_vf-userdebug
